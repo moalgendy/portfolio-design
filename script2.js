1b7627a5f2c30010b4c2f9bb04e054b7d15adb7f
mhmad12354 <86393471+mhmad12354@users.noreply.github.com>
2022-01-04 14:21:58 +0200

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,12 @@
+// ------------loading--------------- //
+
+
+window.onload = function(){
+    var spinner = document.getElementById("sp");
+    document.body.style.overflow = "hidden";
+
+    setTimeout(function() {
+        spinner.style.display = "none";
+        document.body.style.overflow = "auto";
+    }, 3000);
+}
